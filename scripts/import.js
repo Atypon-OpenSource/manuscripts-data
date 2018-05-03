7ddc1c159793b10e64a6f1e3a6e1234eae5fe5c3
Alf Eaton <aeaton@atypon.com>
2018-05-03 22:24:41 +0100

    Initial commit

---
+++ scripts/import.js
@@ -0,0 +1,26 @@
+require('dotenv-safe').config()
+const axios = require('axios')
+const Promise = require('bluebird')
+const fs = require('fs-extra')
+const globby = require('globby')
+
+Promise.map(
+  globby('data/*.json'),
+  file => fs.readJSON(file).then(docs => axios({
+    method: 'post',
+    url: `${process.env.SYNC_GATEWAY_URL}${process.env.SHARED_DATA_BUCKET}/_bulk_docs`,
+    auth: {
+      username: `${process.env.SHARED_DATA_BUCKET}_admin`,
+      password: process.env.COUCHBASE_RBAC_PASSWORD,
+    },
+    data: ({
+      docs,
+      new_edits: true
+    }),
+  })),
+  { concurrency: 5 }
+).then(() => {
+  console.log('finished importing')
+}).catch(error => {
+  console.error(error.message)
+})
