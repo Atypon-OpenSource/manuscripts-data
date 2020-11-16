87a25e5bc93adc03a18a5f2bc0c77b5433649e29
Alf Eaton <aeaton@atypon.com>
2020-11-16 15:14:29 +0000

    Remove unused scripts
    Remove scripts that are no longer used for publishing data to S3 and building the Docker image.

--- scripts/invalidate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # exit if any step fails
-set -u # exit if a variable isn't set
-
-aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths "/data/*"
